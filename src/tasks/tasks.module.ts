e63ca7eb953865f4c3b8b854c7dc1121793eef7b
Pat Needham <patrick.h.needham@gmail.com>
2024-02-15 18:05:12 -0500

    cleanup

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
-// scaffolded by nestjs: `nest g module tasks`
-
 @Module({
   controllers: [TasksController],
   providers: [TasksService],
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
-// scaffolded by nestjs: `nest g service tasks --no-spec`
-
 @Injectable()
 export class TasksService {}
