a65606cace5cf97faf4b32bd38686cf9a17a0632
Pat Needham <patrick.h.needham@gmail.com>
2024-02-16 13:08:28 -0500

    sign jwt token on sign in

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,11 +4,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './dto/jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private usersRepository: UsersRepository,
+    private jwtService: JwtService,
   ) {}
 
   // service is business logic: sign up
@@ -17,12 +20,16 @@ export class AuthService {
     return await this.usersRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialsDto;
     const user = await this.usersRepository.getUserByUsername(username);
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'success';
+      const payload: JwtPayload = { username };
+      const accessToken = this.jwtService.sign(payload);
+      return { accessToken };
     } else {
       throw new UnauthorizedException('Invalid credentials');
     }
