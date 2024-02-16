d555ff1b140f6b537fe7da249e6d571dc951050e
Pat Needham <patrick.h.needham@gmail.com>
2024-02-16 11:57:28 -0500

    repository injection

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UsersRepository } from './users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User) private usersRepository: UsersRepository,
+  ) {}
+}
