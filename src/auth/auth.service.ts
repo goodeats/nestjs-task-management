d63e08d05abe77e68740356fbe5317ba4f310893
Pat Needham <patrick.h.needham@gmail.com>
2024-02-16 12:09:36 -0500

    /auth/signup creates a new user

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { UsersRepository } from './users.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User) private usersRepository: UsersRepository,
   ) {}
+
+  // service is business logic: sign up
+  // repository is data access: create user
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    return await this.usersRepository.createUser(authCredentialsDto);
+  }
 }
--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,6 @@
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 // Creating and Using Custom Repositories in NestJS with TypeORM 0.3
 // https://tech.durgadas.in/creating-and-using-custom-repositories-in-nestjs-with-typeorm-0-3-c7ac9548ad99
@@ -7,6 +8,13 @@ import { User } from './user.entity';
 
 export interface UsersRepository extends Repository<User> {
   this: Repository<User>;
+  createUser(authCredentialsDto: AuthCredentialsDto): Promise<void>;
 }
 
-export const customUsersRepository: Pick<UsersRepository, any> = {};
+export const customUsersRepository: Pick<UsersRepository, any> = {
+  createUser(this: Repository<User>, authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+    this.save(user);
+  },
+};
