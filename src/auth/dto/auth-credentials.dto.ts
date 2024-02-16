f6c3e2d94cfe4f71d802bbfe7d2a64aaca9f8c96
Pat Needham <patrick.h.needham@gmail.com>
2024-02-16 12:13:58 -0500

    auth validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,4 +1,21 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
 export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
+  // Password must contain at least
+  // - one lowercase letter
+  // - one uppercase letter
+  // - and one number
+  // Minimum length of 8 characters
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/, {
+    message: 'password too weak',
+  })
   password: string;
 }
